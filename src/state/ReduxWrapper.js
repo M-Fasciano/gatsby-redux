a6599aeeb2fdc5d80eedd0dfc6e00cf8107cd3c6
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2020-05-11 09:04:09 +0100

    Add local storage to save state of counter

--- src/state/ReduxWrapper.js
+++ src/state/ReduxWrapper.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
-import { createStore as reduxCreateStore } from 'redux'
+import { createStore } from 'redux'
+import { loadState, saveState } from '../reducers/localStorage'
 import rootReducer from '../reducers'
+import throttle from 'lodash.throttle'
 
-const createStore = () => reduxCreateStore(rootReducer, composeWithDevTools())
+const persistedState = loadState()
+const store = createStore(
+  rootReducer,
+  persistedState,
+  composeWithDevTools()
+)
+
+store.subscribe(throttle(() => {
+  saveState({
+    counter: store.getState().counter
+  })
+}, 1000))
 
 export default ({ element }) => (
-  <Provider store={createStore()}>{element}</Provider>
+  <Provider store={store}>{element}</Provider>
 )
